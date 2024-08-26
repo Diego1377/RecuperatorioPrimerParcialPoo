ac9739e579b9579b5ca4ac0f09532e64e45a9855
diego biancofiore <diegobiancofiore23@gmail.com>
2024-08-26 20:03:16 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio04.Datos/RepositorioNumeros.cs
@@ -0,0 +1,116 @@
+﻿using Ejercicio04.Entidades;
+
+namespace Ejercicio04.Datos
+{
+    public class RepositorioNumeros
+    {
+        private NumeroDivDosTres[]? numeros;
+        private int cantidad;
+        public int GetCantidad() => cantidad;
+        public RepositorioNumeros(int Cantidad)
+        {        
+            cantidad = Cantidad;
+            numeros = new NumeroDivDosTres[cantidad];          
+        }
+        public RepositorioNumeros() : this(5) { }
+        private bool EstaCompleto() => numeros!.All(n => !(n is null));
+        private bool EstaVacio() => numeros!.All(n => n is null);
+        private bool ExisteNumero(NumeroDivDosTres nBuscado) => numeros!.Any(n => n == nBuscado);
+
+        public (bool, string) Agregar(NumeroDivDosTres nAgregar)
+        {
+            if (EstaCompleto())
+            {
+                return (false, "El repositorio está completo.");
+            }
+            if (!nAgregar.EsValido())
+            {
+                return (false, "Numero Divisible por 2 y 3 no valido");
+            }
+            if (ExisteNumero(nAgregar))
+            {
+                return (false, "El número ya existe en el repositorio.");
+            }
+            for (int i = 0; i <= numeros!.Length; i++)
+            {
+                if (numeros[i] is null)
+                {
+                    numeros[i] = nAgregar;
+                    return (true, $"Número agregado en la posicion {i}.");
+                }
+            }
+            return (false, "No se puede agregar");
+        }
+        public (bool, string) QuitarNumero(NumeroDivDosTres nQuitar)
+        {
+            if (EstaVacio())
+            {
+                return (false, "Repo sin elementos");
+            }
+            if (!ExisteNumero(nQuitar))
+            {
+                return (false, "numero inexistente");
+            }
+            for (int i = 0; i < numeros!.Length; i++)
+            {
+                if (numeros[i].Equals(nQuitar))
+                {
+                    numeros[i] = null;
+                    return (true, "Número eliminado correctamente.");
+                }
+            }
+            return (false, "El número no se encontró en el repositorio.");
+        }
+        public NumeroDivDosTres ObtenerElemento(int index)
+        {
+            if (index < 0 || index > cantidad - 1)
+            {
+                throw new IndexOutOfRangeException("Indice fuera del rango permitido");
+            }
+            return numeros![index];
+
+        }
+        public string ObtenerTodosLosNumeros()
+        {
+            string resultado = string.Empty;
+            if (EstaVacio())
+            {
+                return "No hay elementos almacenados todavía.";
+            }
+            else
+            {
+                for (int i = 0; 1 <= numeros!.Length; i++)
+                {
+                    resultado += numeros[1] is null ? "Elemento Nulo" : numeros[i].ToString();
+                    resultado += "\n";
+                }
+            }
+            return resultado;
+
+        }
+        public (bool, int) BuscarNumero(NumeroDivDosTres n)
+        {
+            if (EstaVacio())
+            {
+                return (false, -1);
+            }
+            if (!ExisteNumero(n))
+            {
+                return (false, -1);
+            }
+            for (int i = 0; i <= numeros!.Length; i++)
+            {
+                if (n.Equals(numeros[i]))
+                {
+                    return (true, i);
+                }
+            }
+            return (false, -1);
+        }
+        public static implicit operator int(RepositorioNumeros repo)
+        {
+            if (repo.EstaVacio()) return 0;
+            return repo.numeros!.Sum(x => x.Valor);
+        }
+    }
+}
---
+++ Ejercicio04.Entidades/NumeroDivDosTres.cs
@@ -0,0 +1,42 @@
+﻿namespace Ejercicio04.Entidades
+{
+    public class NumeroDivDosTres
+    {
+        public int Valor { get; set; }
+        public NumeroDivDosTres(int valor)
+        {
+            Valor = valor;
+        }
+        public bool EsValido()
+        {
+            int numero = Valor;
+            return numero % 2 == 0 && numero % 3 == 0;
+        }
+        public override string ToString()
+        {
+            return $"{Valor}";
+        }
+        public static bool operator ==(NumeroDivDosTres n1, NumeroDivDosTres n2)
+        {
+            if (ReferenceEquals(n1, null) || ReferenceEquals(n2, null))
+            {
+                return ReferenceEquals(n1, n2);
+            }
+            return n1.Valor == n2.Valor;
+        }
+        public static bool operator !=(NumeroDivDosTres n1, NumeroDivDosTres n2)
+        {
+            return !(n1 == n2);
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || !(obj is NumeroDivDosTres nd)) return false;
+            return this == nd;
+        }
+        public override int GetHashCode()
+        {
+            return Valor.GetHashCode();
+        }
+
+    }
+}
